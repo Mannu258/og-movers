a81b2e91ba5b2aeb95ea42937f26f70b61236dde
Mandeep <mandeep_mis@mojopanda.com>
2024-04-16 15:24:40 +0530

    working fine

--- ogapp/models.py
+++ ogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class contact(models.Model):
+    name = models.CharField(max_length=60)
+    mobile = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    price = models.IntegerField()
+    from_location = models.TextField()
+    to_location = models.TextField()
+    date = models.DateField()
+    def __str__(self) -> str:
+        return self.name
+
--- ogapp/views.py
+++ ogapp/views.py
@@ -6,4 +6,21 @@ def home(request):
     return render(request,'index.html')
 
 def contact(request):
+    if request.method == "POST":
+       name =  request.POST.get('name')
+       email =  request.POST.get('email')
+       price =  request.POST.get('price')
+       date =  request.POST.get('date')
+       mobile =  request.POST.get('mobile')
+       froml =  request.POST.get('froml')
+       tol =  request.POST.get('tol')
+       print(name)
+       print(email)
+       print(price)
+       print(date)
+       print(mobile)
+       print(froml)
+       print(tol)
+
+
     return render(request,'contact.html')
