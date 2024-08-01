d4964322ef9683c14e3692477323e80416a5fcbc
Your Name <you@example.com>
2024-08-01 18:25:43 +0530

    fix numner

--- ogapp/email.py
+++ ogapp/email.py
@@ -3,7 +3,7 @@ from django.core.mail import EmailMessage
 def send_order_email(name, customer_email, price, date, mobile, froml, tol):
     email = EmailMessage(
         subject='Order Received',
-        body=f"Name:\t{name}\nEmail:\t{customer_email}\nPrice:\t{price}\nDate:\t{date}\nMobile:\t{mobile}\nFrom:\t{froml}\nTo:\t{tol}\n\nRegards,\nOG Movers\n\nContact Us:\nPhone: +1 (123) 456-7890\nEmail: info@ogmovers.com",
+        body=f"Name:\t{name}\nEmail:\t{customer_email}\nPrice:\t{price}\nDate:\t{date}\nMobile:\t{mobile}\nFrom:\t{froml}\nTo:\t{tol}\n\nRegards,\nOG Movers\n\nContact Us:\nPhone: +61 (434) 653-406\nEmail: info@ogmovers.com",
         to=['mandeepkumarmannu123@gmail.com','ogmovers1@gmail.com','ogmovers2@gmail.com','priyanshuv007@gmail.com']
         # to=['mandeepkumarmannu123@gmail.com']
     )
@@ -14,7 +14,7 @@ def send_order_email(name, customer_email, price, date, mobile, froml, tol):
         f"Hello {name},\n\nThank you for reaching us. We are working on your inquiry and "
           "will get back to you as soon as possible.\n\nIn the meantime, feel free to check our FAQ section.\n\n"
           "Thank you for your patience,\n OG Movers Support Team\n\n"
-          "Regards,\nOG Movers\n\nContact Us:\nPhone: +1 (123) 456-7890\nEmail: ogmovers1@gmail.com"),
+          "Regards,\nOG Movers\n\nContact Us:\nPhone: +61 (434) 653-406\nEmail: ogmovers1@gmail.com"),
     to=[f'{customer_email}']
 )
 
